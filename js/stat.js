42e13785661a54e23a728f42546a2175e6593d05
Марианна Эльдарова <sofi.mishel2014@yandex.ru>
2019-09-23 01:09:42 +0300

    Холст на канвасе 2.0

--- js/stat.js
+++ js/stat.js
@@ -2,32 +2,37 @@
 
 
 
-window.renderStatistics = function(ctx, names, times){
-	
-	var CLOUD_X = 100;
-	var CLOUD_Y = 10;
-	var COLUMN_WIDTH = 40;
-	
-	// Функция рендера облака
-	
-	var renderCloud = function (x, y, color) {
-	var CLOUD_WIDTH = 420;
+// Функция поиска максимального значения
+function getMaxElement(arr) {
+  return Math.max.apply(null, arr);
+}
+// Функция генерации рандомной насыщенности цвета
+var makeRandomColor = function () {
+  return ('hsl(240, ' + (Math.random() * 100) + '%, 50%)');
+};
+
+window.renderStatistics = function (ctx, players, times) {
+  var CLOUD_X = 100;
+  var CLOUD_Y = 10;
+  var COLUMN_WIDTH = 40;
+
+  // Функция для рендера облака (координаты, цвет)
+  var renderCloud = function (x, y, color) {
+    var CLOUD_WIDTH = 420;
     var CLOUD_HEIGHT = 270;
 
     ctx.fillStyle = color;
     ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-	
-};
-	
-	var renderCloudWithShadow = function () {
-    var GAP = 10;
-    var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
-    var CLOUD_COLOR = '#ffffff';
-   
+  };
+
+  var renderCloudWithShadow = function () {
+    var GAP = 10; // Отступ от края облака
+    var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)'; // Цвет тени
+    var CLOUD_COLOR = '#ffffff'; // Цвет облака
+    // Передали координаты для облака с тенью + цвет, так же для просто облака
     renderCloud(CLOUD_X + GAP, CLOUD_Y + GAP, SHADOW_COLOR);
     renderCloud(CLOUD_X, CLOUD_Y, CLOUD_COLOR);
   };
-  
   // Функция отрисовки текста
   var renderText = function (strings, color, x, y) {
     var FONT_PROPERTIES = ['16px', 'PT Mono']; // Массив параметров текста
@@ -44,8 +49,7 @@ window.renderStatistics = function(ctx, names, times){
       ctx.fillText(string, x, y + (FONT_HEIGHT * i));
     });
   };
-  
-   // Отрисовка текста приветствия
+  // Отрисовка текста приветствия
   var renderTopText = function () {
     var TOP_TEXT = ['Ура вы победили!', 'Список победителей']; // Соответственно, после пересчета количества элементов массива отрисовка нарисует 2 строки вертикально с отступом FONT_HEIGHT
     renderText(TOP_TEXT);
@@ -55,8 +59,8 @@ window.renderStatistics = function(ctx, names, times){
     ctx.fillStyle = color;
     ctx.fillRect(left, bottom - columnHeight, COLUMN_WIDTH, columnHeight);
   };
-  
-   var renderHist = function (columnHeight, left, name, time) {
+
+  var renderHist = function (columnHeight, left, name, time) {
     var BAR_Y = 255; // CLOUD_HEIGHT - GAP - FONT_HEIGHT
     var userTimeY = BAR_Y - columnHeight - 10; // Чтобы координата была вверху - отрицательное число = высоте отрисованного бара + отступ между текстом и баром
     var userNameY = BAR_Y + 15; // Высота бара + высота текста
